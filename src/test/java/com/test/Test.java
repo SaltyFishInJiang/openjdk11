b0ef34fd37906a58aae51e6cf38a490ed9dba323
ryan <ryan@xingren.com>
2021-01-20 12:20:06 +0800

    init

---
+++ src/test/java/com/test/Test.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import java.util.ArrayList;
+
+/**
+ * @author ryan
+ * @since 2021/01/20
+ */
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>();
+        list.add(1);
+    }
+}
